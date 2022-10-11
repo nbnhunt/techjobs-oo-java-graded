35b9fbeadf1ef963579c20bbaf9cfbf788b4c0fd
nbnhunt <nbnhunt@outlook.com>
2022-10-10 20:30:41 -0500

    Pick back up at "Test teh Full Constructor: 3"

--- src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
+++ src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
@@ -1,12 +1,33 @@
 package org.launchcode.techjobs.oo.test;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import org.launchcode.techjobs.oo.*;
+
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Created by LaunchCode
  */
 @RunWith(JUnit4.class)
 public class JobTest {
+
+    @Test
+    public void testSettingJobId() {
+        Job job1 = new Job();
+        Job job2 = new Job();
+        assertNotEquals(job1.getId(), job2.getId());
+    }
+
+    // ASSERTTRUE() METHOD IS WHERE I'M AT 10/10/22
+    // next test should have 5 assert statements of each type...
+    @Test
+    public void testJobConstructorSetsAllFields() {
+        new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        assertTrue(Employer.equals("ACME"));
+
+    }
 
 }
